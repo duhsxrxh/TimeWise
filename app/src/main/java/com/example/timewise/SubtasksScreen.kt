ed38671f358695f9ff7da48e46ba75e25b371f95
duhsxrxh <dasarahn1970@gmail.com>
2023-09-23 13:30:56 +0200

    subtask screen + create subtask screen

--- app/src/main/java/com/example/timewise/SubtasksScreen.kt
+++ app/src/main/java/com/example/timewise/SubtasksScreen.kt
@@ -1,18 +1,26 @@
 package com.example.timewise
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.timewise.databinding.ActivitySignUpScreenBinding
+import com.example.timewise.databinding.ActivitySubtasksScreenBinding
 
 class SubtasksScreen : AppCompatActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        val binding = ActivitySignUpScreenBinding.inflate(layoutInflater)
+        val binding = ActivitySubtasksScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val btnAddSubTask = null
+        binding.btnAddSubTask.setOnClickListener()
+        {
+            var sendIntent = Intent(this, CreateSubtasksScreen :: class.java)
+
+            //takes users tot he SignUp Screen
+            startActivity(sendIntent)
+
+        }
 
     }
 
