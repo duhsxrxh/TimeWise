53a81996f336b74a4e40d6a351b4f0f703b46398
Kuhlekwethu <kuhle0306@gmail.com>
2023-09-25 13:47:27 +0200

    Inflator Pop up

--- app/src/main/java/com/example/timewise/MainActivity.kt
+++ app/src/main/java/com/example/timewise/MainActivity.kt
@@ -2,13 +2,26 @@ package com.example.timewise
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
 import android.widget.EditText
+import android.widget.RelativeLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.timewise.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
 {
+
+    //activity main xml components
+    var button : Button? = null
+    var text : TextView? = null
+
+    //inflated view components
+    var viewButton: Button?= null
+    var inflateText: EditText ?= null
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -37,7 +50,7 @@ class MainActivity : AppCompatActivity()
 
                 Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
-                var sendIntent = Intent(this, SubtasksScreen :: class.java)
+                var sendIntent = Intent(this, HomeScreen :: class.java)
 
                 //takes users to the Home Screen
                 startActivity(sendIntent)
@@ -54,8 +67,51 @@ class MainActivity : AppCompatActivity()
 
         }
 
+        //super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        //activity main xml components
+        button = findViewById(R.id.btnAddSubCategory)
+        text = findViewById(R.id.txtCategoryName)
 
+        button?.setOnClickListener()
+        {
+            //disable button bc it freezes
+            button?.isEnabled = false
+
+            val inflater = LayoutInflater.from(applicationContext)
+            val view = inflater.inflate(R.layout.addcategorylayout, null)
+
+            //container is where we are adding the view
+            val container = findViewById<RelativeLayout>(R.id.homeScreenLayout)
+
+            //create layout parameters for centering the view
+            val layoutParams = RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT)
+
+            //set layout parameters to the view
+            view.layoutParams = layoutParams
+
+            container.addView(view) //display the inflated view
+
+            //inflated layout components
+            viewButton = findViewById(R.id.btnGetText)
+            inflateText = findViewById(R.id.etInflateText)
+
+            viewButton?.setOnClickListener()
+            {
+                val t = inflateText?.text
+                text?.setText(t)
+
+                button?.isEnabled = true
+
+                //remove the inflated view
+                container.removeView(view)
+            }
+        }
     }
 
 }
