60874b8aced14023220ffc3204da9e0132d70caf
duhsxrxh <dasarahn1970@gmail.com>
2023-09-28 09:39:13 +0200

    .

--- app/src/main/java/com/example/timewise/HomeScreen.kt
+++ app/src/main/java/com/example/timewise/HomeScreen.kt
@@ -3,154 +3,141 @@ package com.example.timewise
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.timewise.databinding.ActivityHomeScreenBinding
 import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.LinkedList
 import java.util.Locale
 
-class HomeScreen : AppCompatActivity()
-{
+class HomeScreen : AppCompatActivity() {
 
-    //String []data = {"Hello"};
+    private val data = arrayOf("Hello")  // Use Kotlin array syntax
 
-    val counter = 0;
+    private var counter = 0
 
+    // Activity main XML components
+    private var homeButton: Button? = null
+    private var overviewButton: Button? = null
+    private var activityButton: Button? = null
+    private var profileButton: Button? = null
+    private var mainScreenText: TextView? = null
 
-    //activity main XML components
-    var homeButton : Button ?= null
-    var overviewButton : Button ?= null
-    var activityButton : Button ?= null
-    var profileButton : Button ?= null
-    var mainScreenText : TextView ?= null
+    // Inflated view components
+    private var btnCreatecategory: Button? = null
+    private var inflatedTextCategoryName: EditText? = null
+    private var backImage: ImageView? = null
 
-    //Inflated view components
-    var btnCreatecategory: Button?= null
-    var inflatedTextCategoryName: EditText?= null
-    var backImage: ImageView?= null
-
-
-
-    @Override
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityHomeScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //List<String>() items = new LinkedList<>();
-        //items.add("Code It");
+        val items = LinkedList<String>()  // Use LinkedList to store items
 
-        //RecyclerView recyclerView = findViewById(R.id.recyclerViewCategories);
-        //recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        //DemoAdapter adapter = new DemoAdapter(items);
-        //recyclerView.setAdapter(adapter);
+        items.add("Code It")
 
-        /*findViewById(R.id.add).setOnClickListener(view ->
-        {
-            items.add(data[counter%3]);
-            counter++;
-            adapter.notifyItemInserted(items.size()-1);
-        });*/
+        // Assuming you have already initialized 'items' and 'counter'
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewCategories)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = DemoAdapter(items)
+        recyclerView.adapter = adapter
 
-        //setting the date
+        findViewById<View>(R.id.btnAddCategory).setOnClickListener { view ->
+            items.add(data[counter % 3])
+            counter++
+            adapter.notifyItemInserted(items.size - 1)
+        }
+
+        // Setting the date
         val dateTextView = findViewById<TextView>(R.id.dateTextView)
         val currentDate = getCurrentDate()
         dateTextView.text = currentDate
 
-        //setting the day
+        // Setting the day
         val dayTextView = findViewById<TextView>(R.id.dayTextView)
         val currentDay = getCurrentDay()
         dayTextView.text = currentDay
 
-        //activity main XML components
-        mainScreenText = findViewById(R.id.txtCategoryName)
+        // Activity main XML components
+        mainScreenText = findViewById(R.id.categoryCardName)
         homeButton = findViewById(R.id.btnHomeScreen)
         overviewButton = findViewById(R.id.btnCalenderScreen)
         activityButton = findViewById(R.id.btnActivityScreen)
         profileButton = findViewById(R.id.btnProfileScreen)
 
-        binding.btnAddCategory.setOnClickListener()
-        {
+        binding.btnAddCategory.setOnClickListener {
             val inflater = LayoutInflater.from(applicationContext)
             val view = inflater.inflate(R.layout.show_pop_up, null)
 
-            //container is where we are adding the view
+            // Container is where we are adding the view
             val container = findViewById<ConstraintLayout>(R.id.homeScreenLayout)
 
-            //create layout parameters for centering the view
-            val layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+           /* // Create layout parameters for centering the view
+            val layoutParams = ConstraintLayout.LayoutParams(
+                //ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                //ConstraintLayout.LayoutParams.WRAP_CONTENT
+            )*/
 
-            //set layout params to the view
-            view.layoutParams = layoutParams
+            // Set layout params to the view
+            //view.layoutParams = layoutParams
 
-            //display the inflated view
+            // Display the inflated view
             container.addView(view)
 
-            @Override
-            //inflated layout components
-            btnCreatecategory = findViewById(R.id.btnCreateNewCategory)
-            inflatedTextCategoryName = findViewById(R.id.txtCreateNewCategory)
-            backImage = findViewById(R.id.createACategoryBackground)
+            // Inflated layout components
+            btnCreatecategory = view.findViewById(R.id.btnCreateNewCategory)
+            inflatedTextCategoryName = view.findViewById(R.id.txtCreateNewCategory)
+            backImage = view.findViewById(R.id.createACategoryBackground)
 
-            btnCreatecategory?.setOnClickListener()
-            {
-                //Get the text from the inflated EditText
+            btnCreatecategory?.setOnClickListener {
+                // Get the text from the inflated EditText
                 val newText = inflatedTextCategoryName?.text.toString()
 
-                //Set the text of the main TextView to the new text
+                // Set the text of the main TextView to the new text
                 mainScreenText?.text = newText
 
-                //Remove the inflated view
+                // Remove the inflated view
                 container.removeView(view)
-
             }
-
-
         }
 
-        //opening profile screen button
-        binding.btnProfileScreen.setOnClickListener()
-        {
+        // Opening profile screen button
+        binding.btnProfileScreen.setOnClickListener {
             val profileIntent = Intent(this, ProfileScreen::class.java)
             startActivity(profileIntent)
         }
 
-        //opening overview screen button
-        binding.btnOverviewScreen.setOnClickListener()
-        {
+        // Opening overview screen button
+        binding.btnOverviewScreen.setOnClickListener {
             val overviewIntent = Intent(this, Overview::class.java)
             startActivity(overviewIntent)
         }
 
-        //opening activity screen button
-        binding.btnActivityScreen.setOnClickListener()
-        {
+        // Opening activity screen button
+        binding.btnActivityScreen.setOnClickListener {
             val activityIntent = Intent(this, ActScreen::class.java)
             startActivity(activityIntent)
         }
-
-
     }
 
-    private fun getCurrentDate(): String
-    {
+    private fun getCurrentDate(): String {
         val dateFormat = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
         val currentDate = Date()
         return dateFormat.format(currentDate)
     }
 
-    private fun getCurrentDay(): String
-    {
+    private fun getCurrentDay(): String {
         val dayFormat = SimpleDateFormat("EEEE", Locale.getDefault())
         val currentDay = Date()
         return dayFormat.format(currentDay)
     }
-
 }
